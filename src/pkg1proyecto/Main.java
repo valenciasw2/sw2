8904d3afdaea149a75140991aa1f826ab1a98c99
Daniel <Daniel@172.16.100.195>
2014-08-08 19:48:49 -0500

    Bla

--- src/pkg1proyecto/Main.java
+++ src/pkg1proyecto/Main.java
@@ -4,6 +4,8 @@
  */
 package pkg1proyecto;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Administrator
@@ -12,16 +14,53 @@ public class Main {
 
     /**
      * @param args the command line arguments
+     * EJEMPLO
      */
     public static void main(String[] args) {
-        
-acciones c =  new acciones();
-c.libro();
-c.Actualizacion();
-          
+
+        System.out.println("ingrese lo que desea realizar:"
+                + "para pagos- 1"
+                + "para libros- 2"
+                + "para membresia- 3");
+        Scanner leer = new Scanner(System.in);
+        ejecutarOpcion(leer.nextInt());
     }
-    
-    
-    
-    
+
+    public static void ejecutarOpcion(int opcionSeleccionada) {
+        pago pag = new pago();
+        switch (opcionSeleccionada) {
+
+            case 1: {
+
+                pag.generar_orden_compra();
+                pag.orden_de_entrega_libros();
+                break;
+
+            }
+            case 2: {
+
+                pag.orden_de_entrega_libros();
+
+                break;
+
+            }
+            case 3: {
+
+                pag.activacion();
+
+                break;
+
+            }
+            case 4: {
+
+                pag.video_compra();
+                break;
+            }
+            case 5: {
+                pag.actualizar();
+                break;
+            }
+
+        }
+    }
 }
