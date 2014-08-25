9a81910b6f3bec4fab548e00e47de22ff3dfb339
Daniel <Daniel@172.16.100.195>
2014-08-24 19:44:27 -0500

    DANIEL 1.0.1

--- src/pkg1proyecto/pago.java
+++ src/pkg1proyecto/pago.java
@@ -9,58 +9,50 @@ package pkg1proyecto;
  * @author Administrator
  */
 public class pago {
-    
-Boolean pago_fisico = true ;
-boolean libro;
-boolean Membresia;
-boolean Mebresia_Actualizacion;
-boolean Video;
 
-public void pagos(){
-    if(pago_fisico==true){
-         this.generar_orden_compra();
-          }
-    if(libro==true){
-       this.orden_de_entrega_libros();
-          }
-    
-    if(Membresia==true){
-        this.activacion();
-          }
-            
-    if(Video==true){
-         this.video_compra();
-           }  
-    if(Mebresia_Actualizacion=true){
-        this.actualizar();
+   /**
+     * public void pagos(){
+     *
+     * if(pago_fisico==true){ this.generar_orden_compra(); } if(libro==true){
+     * this.orden_de_entrega_libros(); }
+     *
+     * if(Membresia==true){ this.activacion(); }
+     *
+     * if(Video==true){ this.video_compra(); } if(Mebresia_Actualizacion=true){
+     * this.actualizar(); }
+     *
+     * }
+     *
+     *
+     */
+    public void generar_orden_compra() {
+
+        int consecutivo = 1;
+        if (consecutivo > 0) {
+            consecutivo += 1;
+        }
+
+        System.out.println("se genera la orden numero:" + consecutivo);
+
     }
-     
-}
 
+    public boolean orden_de_entrega_libros() {
+        System.out.println("se genera el duplicado para el departamento de libros");
+        return true;
+    }
 
-public void generar_orden_compra(){
-    
-    System.out.println("se genera la orden numero :25455");
-    
-}
-public boolean orden_de_entrega_libros(){
-   System.out.println("se genera el duplicado");
-   return true;
-}
-public boolean activacion(){
-    System.out.println("su cuenta esta activada");
-    return true;
-}
- public boolean video_compra(){
-    System.out.println("Su video se enviara. por la compra de este se le obsequia un video de primeros auxilios");
-    return true;
-}
- public boolean actualizar(){
-    System.out.println("su informacion esta actualizada");
-    return true;
-}
+    public boolean activacion() {
+        System.out.println("su cuenta esta activada");
+        return true;
+    }
 
+    public boolean video_compra() {
+        System.out.println("Su video se enviara. por la compra de este se le obsequia un video de primeros auxilios");
+        return true;
+    }
 
-
-    
+    public boolean actualizar() {
+        System.out.println("su informacion esta actualizada");
+        return true;
+    }
 }
